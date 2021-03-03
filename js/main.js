aa78e2becf2456f714f198d387ce36e59ca5b2cd
Karthik D <krtdvn@gmail.com>
2021-03-03 12:29:18 +0530

    u

--- js/main.js
+++ js/main.js
@@ -19,6 +19,9 @@ function init() {
         return record.active && record.active.toLowerCase()=="true";
       });
       
+      document.querySelector("#loading").style.display = "none";
+      document.querySelector("#portalApp").style.display = "grid";
+      
       
       appObj = {
         data() {
@@ -29,6 +32,8 @@ function init() {
               variety: "",
               processing: "",
               quantity: 25,
+              minAflatoxin: 0,
+              maxAflatoxin: 100
             }
           };
         },
@@ -38,9 +43,19 @@ function init() {
             
             var commodity = this.filters.commodity||"";
             var variety = this.filters.variety||"";
+            if(!commodity) variety="";
             var processing = this.filters.processing||"";
             var quantity = parseInt(this.filters.quantity||0);
+            var minAflatoxin = parseInt(this.filters.minAflatoxin||0);
+            var maxAflatoxin = parseInt(this.filters.maxAflatoxin||100);
+            
+            
             return this.records.filter((record) => {
+
+              if(minAflatoxin > maxAflatoxin){
+                return false;
+              }
+            
               if(commodity && (record.commodity||"").toLowerCase()!=commodity){
                 return false;
               }
@@ -53,8 +68,33 @@ function init() {
               if(quantity && parseInt(record.minimumOrderQuantity||0)>quantity){
                 return false;
               }
+              
+              if(recordMinAflatoxin > recordMaxAflatoxin){
+                return false;
+              }
+            
+              
+              var recordMinAflatoxin = parseInt(record.aflatoxinMin||0);
+              var recordMaxAflatoxin = parseInt(record.aflatoxinMax||100);
+                            
+              var distinct = minAflatoxin>recordMaxAflatoxin||recordMinAflatoxin>maxAflatoxin;
+              
+              if(distinct){
+                return false;
+              } 
+              
               return true;
             });
+          }
+        },
+        methods: {
+          buy(record) {
+            record.buyClicked = true;
+          },
+          maillink(record) {
+            return `mailto:team@purescanai.com?`
+              + `Subject=${encodeURIComponent("Purchase Enquiry")}`
+              + `&body=${encodeURIComponent("Product ID #" + record.uid + ": " + record.commodity + " from " + record.sellerName)}`;
           }
         }
       }
